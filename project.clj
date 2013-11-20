ec4e20368d9da6f85055b4dc1eb0e1b68e0017e7
we <daniel.slutsky@gmail.com>
2013-11-21 00:39:59 +0200

    file renaming, added deps

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-time "0.6.0"]
+                 [seesaw "1.4.4"]
+                 [incanter "1.5.4"]
+                 [com.cemerick/pomegranate "0.2.0"]])
