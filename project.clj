1a74960776bc57028c01f22a1364a839b95f8674
we <we@we-desktop.(none)>
2013-12-05 23:34:10 +0200

    changed project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-utils "0.1.0-SNAPSHOT"
+(defproject daslu/clj-utils "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
