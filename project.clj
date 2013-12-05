a5c9d09f746305739df1f63ea1b5e7952e1d9403
we <we@we-desktop.(none)>
2013-12-05 23:57:55 +0200

    updated project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject daslu/clj-utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "various clojure utility functions"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
