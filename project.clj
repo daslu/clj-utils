5fd0fc6c011a6758c5f328efec65149484fd58d8
daslu <daniel.slutsky@gmail.com>
2013-12-30 10:44:06 +0200

    added missing dependency

--- project.clj
+++ project.clj
@@ -9,4 +9,5 @@
                  [incanter "1.5.4"]
                  [com.cemerick/pomegranate "0.2.0"]
                  [org.clojure/algo.generic "0.1.1"]
-                 [cc.artifice/clj-ml "0.6.0-SNAPSHOT"]])
+                 [cc.artifice/clj-ml "0.6.0-SNAPSHOT"]
+                 [org.apache.commons/commons-math3 "3.2"]])
